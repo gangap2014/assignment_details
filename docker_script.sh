afec3deb265d4b765f2e63ffcf1603ec523432b5
root <root@docker-server.(none)>
2015-05-14 23:13:16 +0530

    updated script to disable repo expect repo CentOS base

--- docker_script.sh
+++ docker_script.sh
@@ -12,7 +12,8 @@ echo "tomcat location" $tomcat_loc
 uniq_id=`date +%Y%m%d%H%M%S`
 docker run -d -t -i -p 80:8080 -e TOMCAT_HOME=$tomcat_loc --name test_lab_$uniq_id -v /root/puppet:/puppet devopsil/puppet /bin/bash
 container_id=`docker ps -a | grep -i test_lab_$uniq_id | awk -F' ' '{print $1}'`
-docker exec $container_id yum install -y tar
+###Added option in yum to keep enable only base repo
+docker exec $container_id yum --disablerepo=* --enablerepo=base install -y tar
 docker exec $container_id  puppet apply --modulepath=/puppet/modules /puppet/manifests/site.pp --logdest=/var/log/test_lab_$uniq_id.log
 
 docker exec $container_id /bin/cp /puppet/JPetStore.war $tomcat_loc/webapps/.
